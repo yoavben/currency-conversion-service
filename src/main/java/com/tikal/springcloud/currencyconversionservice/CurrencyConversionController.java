a211e3d694bc90a1c0a0289b73bdc5ff9bc29899
yoavsetoo <yoav@setoo.com>
2019-02-14 09:36:19 +0200

    step18

---
+++ src/main/java/com/tikal/springcloud/currencyconversionservice/CurrencyConversionController.java
@@ -0,0 +1,27 @@
+package com.tikal.springcloud.currencyconversionservice;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyConversionController {
+
+    @GetMapping("currency-converter/form/{from}/to/{to}/{quantity}")
+    public CurrencyConversionBean convertCurrency(@PathVariable String from, @PathVariable String to, @PathVariable BigDecimal quantity) {
+
+        CurrencyConversionBean currencyConversionBean = new CurrencyConversionBean();
+        currencyConversionBean.setId(1L);
+        currencyConversionBean.setFrom("USD");
+        currencyConversionBean.setTo("NIS");
+        currencyConversionBean.setCurrencyMultiple(BigDecimal.TEN);
+        currencyConversionBean.setQuantity(BigDecimal.TEN);
+        currencyConversionBean.setPort(0);
+        currencyConversionBean.setTotalCalculatedAmount(BigDecimal.TEN);
+        return currencyConversionBean;
+    }
+
+}
