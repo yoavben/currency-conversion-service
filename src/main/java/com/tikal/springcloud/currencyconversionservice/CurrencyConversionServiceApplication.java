872254c977edce893926f0e21fb6e69703b9be5c
yoavben <yoav@tikalk.com>
2019-02-16 11:46:57 +0200

    step22

--- src/main/java/com/tikal/springcloud/currencyconversionservice/CurrencyConversionServiceApplication.java
+++ src/main/java/com/tikal/springcloud/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -7,7 +7,6 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.tikal.springcloud.currencyconversionservice")
-@EnableDiscoveryClient
 public class CurrencyConversionServiceApplication {
 
 	public static void main(String[] args) {
